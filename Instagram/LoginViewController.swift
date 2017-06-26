57a59240d763c9a38746ba7fa365c11a9da88f02
Pavani Malli <pavanim@fb.com>
2017-06-26 15:35:21 -0700

    Configured actions for the signup and login buttons along with an error alert

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -7,9 +7,87 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+   
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func registerUser(_ sender: UIButton) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text ?? ""
+        newUser.password = passwordField.text ?? ""
+        
+        if usernameField.text!.isEmpty || passwordField.text!.isEmpty {
+            self.usernameError()
+        }
+        else{
+            newUser.signUpInBackground {(success: Bool, error: Error?) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    
+                } else {
+                    print("User Resgistered Succesfully")
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                }
+                
+            }
+        }
+
+    }
+    
+    
+    
+    @IBAction func loginUser(_ sender: UIButton) {
+        let username = usernameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        
+        if username.isEmpty || password.isEmpty {
+            self.usernameError()
+        }
+        else {
+            PFUser.logInWithUsername(inBackground: username, password: password) { (user:PFUser?, error:Error?) in
+                if let error = error {
+                    print("User log in failed: \(error.localizedDescription)")
+                } else {
+                    print("User logged in succesfully")
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                }
+            }
+        }
+
+    }
+    
+    
+    
+    func usernameError() {
+        let alertController = UIAlertController(title: "Empty username or password", message: "Please enter a username and password", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+        }
+        // add the cancel action to the alertController
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true) {
+            // optional code for what happens after the alert controller has finished presenting
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +100,6 @@ class LoginViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
