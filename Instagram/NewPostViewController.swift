6203636927fcceb476c000df9ba0d359ff6b822a
Pavani Malli <pavanim@fb.com>
2017-06-26 16:23:30 -0700

    Modally present photo library

--- Instagram/NewPostViewController.swift
+++ Instagram/NewPostViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class NewPostViewController: UIViewController {
+class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        print("opened")
+        self.present(vc, animated: true, completion: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -21,15 +29,22 @@ class NewPostViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        dismiss(animated: true, completion: nil)
     }
-    */
+
+    
+
+
+
+
+
+
+
+
+
 
 }
