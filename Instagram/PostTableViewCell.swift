5cda4f5199ed52210d1a2bf231c39bf0a9ed68ea
Pavani Malli <pavanim@fb.com>
2017-06-27 16:07:03 -0700

    Created tableView and cell UI elements for a Instagram post

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Pavani Malli on 6/27/17.
+//  Copyright © 2017 Pavani Malli. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
