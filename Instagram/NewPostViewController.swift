37d6111240da37c6daafa87fac227a3cf9d1bf90
Pavani Malli <pavanim@fb.com>
2017-06-27 14:30:53 -0700

    Added camera roll selector

--- Instagram/NewPostViewController.swift
+++ Instagram/NewPostViewController.swift
@@ -10,20 +10,19 @@ import UIKit
 
 class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var captionTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let vc = UIImagePickerController()
-        vc.delegate = self
-        vc.allowsEditing = true
-        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        
-        print("opened")
-        self.present(vc, animated: true, completion: nil)
-
+        self.openCameraRoll()
         // Do any additional setup after loading the view.
     }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,18 +30,29 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        photoImageView.image = originalImage
         
         dismiss(animated: true, completion: nil)
     }
 
     
+    @IBAction func selectPhoto(_ sender: UIButton) {
+        self.openCameraRoll()
+    }
+
+    func openCameraRoll(){
+    let vc = UIImagePickerController()
+    vc.delegate = self
+    vc.allowsEditing = true
+    vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+    
+    print("opened")
+    self.present(vc, animated: true, completion: nil)
+    }
 
 
-
-
-
-
+   
 
 
 
---
+++ Instagram/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Instagram
+//
+//  Created by Pavani Malli on 6/27/17.
+//  Copyright © 2017 Pavani Malli. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
