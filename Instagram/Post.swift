9c2ed2e6f017bf24728d48fe640d737635497b5d
Pavani Malli <pavanim@fb.com>
2017-06-27 15:31:35 -0700

    Created Post model object with functions to resize image file and submit a post to Parse

--- Instagram/Post.swift
+++ Instagram/Post.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import Parse
 
 class Post: NSObject {
+    
+    class func postUserImage(image: UIImage!, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
+        
+        print("creating post object")
+       
+        let post = PFObject(className: "Post")
+        
+        print("saving media!")
+        post["media"] = getPFFileFromImage(image: image)
+        post["author"] = PFUser.current()
+        post["caption"] = caption
+        post["likesCount"] = 0
+        post["commentsCount"] = 0
+        
+        print("about to save!")
+        post.saveInBackground(block: completion)
+        
+    }
+    
+    
+    class func getPFFileFromImage(image: UIImage!) -> PFFile? {
+        // check if image is not nil
+        print("converting image!")
+        if let image = image {
+            // get image data and check if that is not nil
+            if let imageData = UIImagePNGRepresentation(image) {
+                return PFFile(name: "image.png", data: imageData as Data)
+            }
+        }
+        return nil
+    }
 
 }
