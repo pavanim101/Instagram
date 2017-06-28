dbf8fc8c4b664f4b45136739963cc75174be9266
Pavani Malli <pavanim@fb.com>
2017-06-28 15:16:40 -0700

    Created cell for profileCollectionView

--- Instagram/PostCollectionViewCell.swift
+++ Instagram/PostCollectionViewCell.swift
@@ -7,7 +7,19 @@
 //
 
 import UIKit
+import ParseUI
+import Parse
 
 class PostCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var postImageView: PFImageView!
+    
+    var instagramPost: PFObject! {
+        didSet {
+            self.postImageView.file = instagramPost["media"] as? PFFile
+            self.postImageView.loadInBackground()
+        }
+    }
     
 }
