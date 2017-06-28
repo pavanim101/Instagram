a27af9d0bb29e88746e891cd06c2300899eb79d6
Pavani Malli <pavanim@fb.com>
2017-06-28 15:16:17 -0700

    Added collection view for images posted by user

---
+++ Instagram/PostCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Pavani Malli on 6/28/17.
+//  Copyright © 2017 Pavani Malli. All rights reserved.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+}
--- Instagram/ProfileViewController.swift
+++ Instagram/ProfileViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 import Parse
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var posts: [PFObject] = []
+    
+  
+    @IBOutlet weak var userLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+       
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.queryParse), userInfo: nil, repeats: true)
 
-        // Do any additional setup after loading the view.
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellsPerLine: CGFloat = 3;
+        let interItemSpacing = layout.minimumInteritemSpacing
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacing
+        layout.itemSize = CGSize(width: width, height: width * 1)
+        
+        let user = PFUser.current()
+        userLabel.text = user!.username
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,15 +57,54 @@ class ProfileViewController: UIViewController {
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.posts.count
+        
     }
-    */
+
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostCell", for: indexPath) as! PostCollectionViewCell
+        
+
+        
+        cell.instagramPost = posts[indexPath.row]
+        
+        return cell
+    
+    }
+   
+    
+    func queryParse() {
+        let query = PFQuery(className: "Post")
+        query.addDescendingOrder("createdAt")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
+            if let posts = posts {
+                self.posts = posts
+                self.collectionView.reloadData()
+                
+            }
+            else {
+                print(error!.localizedDescription)
+            }
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! PostCollectionViewCell
+        if let indexPath = collectionView.indexPath(for: cell){
+            let post = posts[indexPath.row]
+            let postDetailViewController = segue.destination as! DetailViewController
+            postDetailViewController.post = post
+        }
+
+}
 
 }
