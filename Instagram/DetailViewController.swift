e79b700fca00ec4e74d50201df4d38aa73f940af
Pavani Malli <pavanim@fb.com>
2017-06-28 14:28:41 -0700

    Added detail view for posts in feed

---
+++ Instagram/DetailViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DetailViewController.swift
+//  Instagram
+//
+//  Created by Pavani Malli on 6/28/17.
+//  Copyright © 2017 Pavani Malli. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class DetailViewController: UIViewController {
+
+    var post: PFObject!
+    
+    
+    @IBOutlet weak var userLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: PFImageView!
+    
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    
+    @IBOutlet weak var likesLabel: UILabel!
+    
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    var instagramPost: PFObject! {
+        didSet {
+            self.photoImageView.file = instagramPost["media"] as? PFFile
+            self.photoImageView.loadInBackground()
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let post = post {
+            let user = post["author"] as! PFUser
+            userLabel.text = user.username!
+            
+            let caption = post["caption"] as! String
+            captionLabel.text = caption
+            
+            let likeCount = post["likesCount"] as! Int
+            likesLabel.text = String(likeCount) + " likes"
+            
+            self.instagramPost = post
+            
+            //let timestamp = post["_created_at"] as! Date
+            
+            
+
+        }
+        
+        
+        
+        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
