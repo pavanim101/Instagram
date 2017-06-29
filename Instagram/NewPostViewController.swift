e399b9ffbcd2056b8d234f7cbc93830f5d576c39
Pavani Malli <pavanim@fb.com>
2017-06-29 15:24:58 -0700

    Added camera picker option

--- Instagram/NewPostViewController.swift
+++ Instagram/NewPostViewController.swift
@@ -19,6 +19,8 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     var captionString: String!
     
+    var sourceType: String!
+    
     
     @IBAction func tapGesture(_ sender: Any) {
         view.endEditing(true)
@@ -28,10 +30,22 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.openCameraRoll()
         
-
-        // Do any additional setup after loading the view.
+        self.photoImageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+        
+        self.photoImageView.layer.borderWidth = 2
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            vc.sourceType = .camera
+        } else {
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
     }
 
    
@@ -46,7 +60,7 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
         let imageData = UIImageJPEGRepresentation(originalImage, 0.2)!
 
         self.photoImageView.image = originalImage
-        
+        self.photoImageView.layer.borderColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1.0).cgColor
         self.photoToPost = UIImage(data: imageData)
         
         dismiss(animated: true, completion: nil)
@@ -54,20 +68,36 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
     
        
     @IBAction func selectPhoto(_ sender: UIButton) {
-        self.openCameraRoll()
+        self.openPhotoSource(source: "photoLibrary")
     }
 
-    func openCameraRoll(){
-    let vc = UIImagePickerController()
-    vc.delegate = self
-    vc.allowsEditing = true
-    vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
     
-    print("opened")
-    self.present(vc, animated: true, completion: nil)
-    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            self.openPhotoSource(source: "camera")
+        } else {
+            self.cameraError()
+        }
     }
-
+    
+    func openPhotoSource(source:String){
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+    
+        
+        if source == "camera" {
+            vc.sourceType = UIImagePickerControllerSourceType.camera
+        } else{
+            vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    
+    
+    
     @IBAction func createPost(_ sender: UIButton) {
         MBProgressHUD.showAdded(to: self.view, animated: true)
 
@@ -88,6 +118,21 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
         captionTextField.text = ""
     }
 
+    func cameraError() {
+        let alertController = UIAlertController(title: "Camera not available", message: "Please select from photo library", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+        }
+        // add the cancel action to the alertController
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true) {
+            // optional code for what happens after the alert controller has finished presenting
+        }
+        
+    }
+    
 
 
 
