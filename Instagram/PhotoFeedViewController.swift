b109365bbf469f83700670eed8596a106ff88b56
Pavani Malli <pavanim@fb.com>
2017-06-29 11:58:11 -0700

    Added infinite scrolling and activity indicator

--- Instagram/PhotoFeedViewController.swift
+++ Instagram/PhotoFeedViewController.swift
@@ -6,8 +6,7 @@
 //  Copyright © 2017 Pavani Malli. All rights reserved.
 //
 
-//TODO: click on username for profile, likes, posts specific to user
-
+//TODO:  likes, progress loading, infinite scrolling, camera/filters, auto layout, aesthetics
 import UIKit
 import Parse
 import ParseUI
@@ -15,10 +14,19 @@ import ParseUI
 class PhotoFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
     @IBOutlet weak var postsTableView: UITableView!
     
     var posts: [PFObject] = []
-
+    var isMoreDataLoading = false;
+    var queryLimit: Int = 20
+    
+    override func viewWillAppear(_ animated: Bool) {
+        activityIndicator.startAnimating()
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,21 +69,26 @@ class PhotoFeedViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostTableViewCell
         
-       
         let user = posts[indexPath.row]["author"] as! PFUser
         cell.userNameLabel.text = user.username!
 
         let caption = posts[indexPath.row]["caption"] as! String
         cell.captionLabel.text = caption
         
+        
         let likeCount = posts[indexPath.row]["likesCount"] as! Int
-        cell.likesLabel.text = String(likeCount) + " likes"
+        if likeCount == 1 {
+        cell.likesLabel.text = String(likeCount) + " like"
+            } else{
+                cell.likesLabel.text = String(likeCount) + " likes"
+        }
         
         cell.instagramPost = posts[indexPath.row]
         
+        cell.currentID = posts[indexPath.row].objectId
+        
         return cell
     }
-    
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -86,14 +99,26 @@ class PhotoFeedViewController: UIViewController, UITableViewDelegate, UITableVie
             postDetailViewController.post = post
         }
         
-        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading){
+            let scrollViewContentHeight = postsTableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - postsTableView.bounds.size.height
+            
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && postsTableView.isDragging) {
+                isMoreDataLoading = true
+                self.queryLimit += 20
+                
+            }
+        }
     }
     
     func queryParse() {
         let query = PFQuery(className: "Post")
         query.addDescendingOrder("createdAt")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = self.queryLimit
         
         query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
             if let posts = posts {
@@ -105,7 +130,9 @@ class PhotoFeedViewController: UIViewController, UITableViewDelegate, UITableVie
                 print(error!.localizedDescription)
             }
         }
-    
+        
+        self.isMoreDataLoading = false
+        activityIndicator.stopAnimating()
     }
     
 }
